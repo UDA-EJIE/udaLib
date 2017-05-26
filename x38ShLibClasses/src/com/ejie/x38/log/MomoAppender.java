4071d1f36a359616f03c62e81d33f90a89d390ba
enekoza <enekoza@gmail.com>
2017-05-26 12:12:13 +0200

    Modificaciones en el adaptador de Momo
    Se ha modificado la implementación en el adaptador de Momo para mejorar su funcionamiento.

--- x38ShLibClasses/src/com/ejie/x38/log/MomoAppender.java
+++ x38ShLibClasses/src/com/ejie/x38/log/MomoAppender.java
@@ -12,10 +12,6 @@ public class MomoAppender extends AppenderBase<ILoggingEvent>{
 
 	private Layout<ILoggingEvent> layout;
 	
-	
-
-	
-	private W43taMomoTraceClient mtc; 
 	/**
 	 * SERVICIO
 	 */
@@ -46,25 +42,6 @@ public class MomoAppender extends AppenderBase<ILoggingEvent>{
 	private Boolean doMomo;
 	
 	
-	@Override
-    public void start() {
-        try {
-        	if(this.doMomo && !this.develomentMode){
-        		this.mtc = W43taMomoTraceClient.getInstance(
-					  this.servicio
-					, this.app
-					, this.securityTokenId
-					, this.w43taEndpointUri
-					, this.w43taEndpointPort
-					, this.develomentMode);
-        	}	
-            
-        } catch (Exception e) {
-            addStatus(new ErrorStatus("Failed to initialize MOMO", this, e));
-            return;
-        }
-        super.start();
-    }
 	
 	public void setServicio(String servicio) {
 		this.servicio = servicio;
@@ -104,24 +81,45 @@ public class MomoAppender extends AppenderBase<ILoggingEvent>{
 		this.layout = layout;
 	}
 
+	private W43taMomoTraceClient getMomoTraceClient(String servicio, String app, String token, String serviceEndPoint, int port, boolean isLocal){
+		try{
+			return W43taMomoTraceClient.getInstance(servicio, app, securityTokenId, w43taEndpointUri, w43taEndpointPort, develomentMode);
+		} catch (Exception e) {
+            addStatus(new ErrorStatus("Failed to initialize MOMO", this, e));
+            return null;
+        }
+	}
+						
+	
 	@Override
 	protected void append(ILoggingEvent event) {
 		
 		if(this.doMomo && !this.develomentMode){
-		
-			switch(event.getLevel().levelInt){
-			case Level.DEBUG_INT:
-				this.mtc.debug(this.layout.doLayout(event));
-				break;
-			case Level.INFO_INT:
-				this.mtc.info(this.layout.doLayout(event));
-				break;
-			case Level.WARN_INT:
-				this.mtc.warning(this.layout.doLayout(event));
-				break;
-			case Level.ERROR_INT:
-				this.mtc.error(this.layout.doLayout(event));
-				break;
+			
+			W43taMomoTraceClient mtc = this.getMomoTraceClient(
+					this.servicio,
+					this.app,
+					this.securityTokenId,
+					this.w43taEndpointUri,
+					this.w43taEndpointPort,
+					this.develomentMode);
+			
+			if (mtc!=null){
+			
+				switch(event.getLevel().levelInt){
+				case Level.DEBUG_INT:
+					mtc.debug(this.layout.doLayout(event));
+					break;
+				case Level.INFO_INT:
+					mtc.info(this.layout.doLayout(event));
+					break;
+				case Level.WARN_INT:
+					mtc.warning(this.layout.doLayout(event));
+					break;
+				case Level.ERROR_INT:
+					mtc.error(this.layout.doLayout(event));
+					break;
+				}
 			}
 		}
 	}
