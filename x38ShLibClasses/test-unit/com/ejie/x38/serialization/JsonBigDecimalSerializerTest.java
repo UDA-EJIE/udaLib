3851820fd2b463d068bd3979a88bb0248de4e9d3
enekoza <enekoza@gmail.com>
2017-10-23 14:11:46 +0200

    Nuevo JsonBigDecimal (de)serializer
    De acuerdo con lo indicado en la issue UDA-EJIE/udaRUP#68 se determina la necesidad de incorporar un serializador y deserializador de Jackson para los tipos BigDecimal.
    
    - Nuevo serializador JsonBigDecimalSerializer
    - Nuevo deserializador JsonBigDecimalDeserializer
    - Nuevos métodos bigDecimalToString y stringToBigDecimal en la clase ObjectConversionManager
    - Test de jUnit para ls clases JsonBigDecimalSerializer y JsonBigDecimalDeserializer
    
    Close UDA-EJIE/udaRUP#68

---
+++ x38ShLibClasses/test-unit/com/ejie/x38/serialization/JsonBigDecimalSerializerTest.java
@@ -0,0 +1,51 @@
+package com.ejie.x38.serialization;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.math.BigDecimal;
+
+import org.junit.Test;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+public class JsonBigDecimalSerializerTest {
+
+	@Test
+	public void testSerialize() throws JsonProcessingException, IOException {
+
+		// Creación de datos de prueba
+		BigDecimal bigDecimal1 = new BigDecimal("123.1234567890123456789");
+		BigDecimal bigDecimal2 = new BigDecimal("123.9876543210987654321");
+
+		// Declaración del resultado esperado
+		String expectedBigDecimal1 = "\"123,1234567890123456789\"";
+		String expectedBigDecimal2 = "\"123,9876543210987654321\"";
+
+		// Ejecución de los métodos de prueba
+		String resultBigDecimal1 = this.serializeBigDecimal(bigDecimal1);
+		String resultBigDecimal2 = this.serializeBigDecimal(bigDecimal2);
+
+		// Verificación del resultado
+		assertTrue(resultBigDecimal1.equals(expectedBigDecimal1));
+		assertTrue(resultBigDecimal2.equals(expectedBigDecimal2));
+
+	}
+
+	private String serializeBigDecimal(BigDecimal bigDecimal) throws IOException {
+		Writer jsonWriter = new StringWriter();
+		SerializerProvider serializerProvider = new ObjectMapper().getSerializerProvider();
+		JsonGenerator jsonGenerator = new JsonFactory().createGenerator(jsonWriter);
+		new JsonBigDecimalSerializer().serialize(bigDecimal, jsonGenerator, serializerProvider);
+		jsonGenerator.flush();
+
+		return jsonWriter.toString();
+
+	}
+}
