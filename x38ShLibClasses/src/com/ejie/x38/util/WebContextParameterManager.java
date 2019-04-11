9dd16b7274f91ae4e1b7f9db128a963efb1c55c7
Lander L <llaparra@euro-help.com>
2019-04-11 12:40:37 +0200

    Quitar una import que no se usa

--- x38ShLibClasses/src/com/ejie/x38/util/WebContextParameterManager.java
+++ x38ShLibClasses/src/com/ejie/x38/util/WebContextParameterManager.java
@@ -26,7 +26,6 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.WebApplicationContext;
 
 /**
