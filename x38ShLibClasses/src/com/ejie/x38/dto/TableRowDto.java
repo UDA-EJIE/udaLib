dfbfa269e7f97995ef88d8f5c7d4b39ac2e41943
xaldama <xabier@hdivsecurity.com>
2021-03-17 17:17:53 +0100

    TableRowDto as SecureIdContainer

--- x38ShLibClasses/src/com/ejie/x38/dto/TableRowDto.java
+++ x38ShLibClasses/src/com/ejie/x38/dto/TableRowDto.java
@@ -3,12 +3,14 @@ package com.ejie.x38.dto;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.hdiv.services.SecureIdContainer;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class TableRowDto<T> {
+public class TableRowDto<T> implements SecureIdContainer {
 	
 	@JsonIgnore
 	private Map<String, String> pkMap = new HashMap<String, String>();
