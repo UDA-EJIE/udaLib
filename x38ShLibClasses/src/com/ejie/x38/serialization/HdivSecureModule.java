a28f2088a56c3514fd8872cce6b31dfe6d4b6fef
xaldama <xabier@hdivsecurity.com>
2021-03-25 00:37:43 +0100

    Serialize null values with HdivSecureModule

--- x38ShLibClasses/src/com/ejie/x38/serialization/HdivSecureModule.java
+++ x38ShLibClasses/src/com/ejie/x38/serialization/HdivSecureModule.java
@@ -20,6 +20,7 @@ import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.ser.BeanPropertyWriter;
 import com.fasterxml.jackson.databind.ser.BeanSerializerModifier;
+import com.fasterxml.jackson.databind.ser.std.NullSerializer;
 
 public class HdivSecureModule extends SimpleModule {
 
@@ -59,8 +60,10 @@ public class HdivSecureModule extends SimpleModule {
 		private void checkSecureIdentifiable(BeanDescription beanDesc,
 				List<BeanPropertyWriter> beanProperties) {
 			if (SecureIdentifiable.class.isAssignableFrom(beanDesc.getBeanClass())) {
+				JsonSerializer<Object> nullSer = NullSerializer.instance;
 				for (int i = 0; i < beanProperties.size(); i++) {
 					BeanPropertyWriter writer = beanProperties.get(i);
+					writer.assignNullSerializer(nullSer);
 					beanProperties.set(i, new NidPropertyWriter(writer, true));
 				}
 			}
@@ -70,9 +73,10 @@ public class HdivSecureModule extends SimpleModule {
 				List<BeanPropertyWriter> beanProperties) {
 			if (SecureIdContainer.class.isAssignableFrom(beanDesc.getBeanClass())) {
 				CustomBeanPropertyWriter newProperty = null; 
+				JsonSerializer<Object> nullSer = NullSerializer.instance;
 				for (int i = 0; i < beanProperties.size(); i++) {
 					BeanPropertyWriter writer = beanProperties.get(i);
-
+					writer.assignNullSerializer(nullSer);
 					TrustAssertion annotation = writer.getAnnotation(TrustAssertion.class);
 					if (annotation != null && annotation.idFor() != null) {
 						newProperty = new CustomBeanPropertyWriter(writer, SecureIdentifiable.NID_PROPERTY);
@@ -104,7 +108,6 @@ public class HdivSecureModule extends SimpleModule {
 						jgen.writeStringField(SecureIdentifiable.NID_PROPERTY, currentNid);
 						provider.setAttribute("NID_OBJECT", null);
 					}
-
 				}
 				
 				super.serializeAsField(bean, jgen, provider);
--- x38ShLibClasses/test-unit/com/ejie/x38/serialization/HdivSecureSerializerTest.java
+++ x38ShLibClasses/test-unit/com/ejie/x38/serialization/HdivSecureSerializerTest.java
@@ -4,15 +4,24 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Date;
+
+import javax.validation.constraints.NotNull;
 
 import org.hdiv.services.SecureIdContainer;
 import org.hdiv.services.SecureIdentifiable;
 import org.hdiv.services.TrustAssertion;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 public class HdivSecureSerializerTest {
 	
@@ -82,6 +91,17 @@ public class HdivSecureSerializerTest {
 		mapper.registerModule(new HdivSecureModule());
 		String serial = mapper.writeValueAsString(new SecuredTrustedIdentifiableParent("name"));
 		assertTrue(serial.contains("\"nid\":\"name_Sec\""));
+
+	}
+	
+	@Test
+	public void testUsuarioSerialize() throws JsonProcessingException, IOException {
+
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.registerModule(new HdivSecureModule());
+		String serial = mapper.writeValueAsString(new Usuario( "1", "nombreu", "ap1", null));
+		assertTrue(serial.contains("\"nid\":\"1\""));
+		assertTrue(serial.contains("\"apellido2\":null"));
 
 	}
 	
@@ -212,5 +232,63 @@ public class HdivSecureSerializerTest {
 			this.secured = secured;
 		}
 
+	}
+	
+	public class Usuario implements java.io.Serializable, SecureIdentifiable<String> {
+
+		private static final long serialVersionUID = 2726038020603366026L;
+		private String id;
+		private String nombre;
+		private String apellido1;
+		private String apellido2;
+		
+		
+	    public Usuario(String id, String nombre, String apellido1, String apellido2) {	
+	        this.id = id;
+	        this.nombre = nombre;
+	        this.apellido1 = apellido1;
+	        this.apellido2 = apellido2;
+	    }
+
+
+		public String getId() {
+			return id;
+		}
+
+
+		public void setId(String id) {
+			this.id = id;
+		}
+
+
+		public String getNombre() {
+			return nombre;
+		}
+
+
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
+
+
+		public String getApellido1() {
+			return apellido1;
+		}
+
+
+		public void setApellido1(String apellido1) {
+			this.apellido1 = apellido1;
+		}
+
+
+		public String getApellido2() {
+			return apellido2;
+		}
+
+
+		public void setApellido2(String apellido2) {
+			this.apellido2 = apellido2;
+		}
+		
 	}
 }
