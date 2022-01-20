7f25d3726e29797073d5ad32a20f1398562afc43
xabier_aldama <xabier.aldama@hdivsecurity.com>
2022-01-20 14:24:37 +0100

    Deleted unneded code

--- x38ShLibClasses/src/com/ejie/x38/hdiv/config/WebConfig.java
+++ x38ShLibClasses/src/com/ejie/x38/hdiv/config/WebConfig.java
@@ -10,52 +10,11 @@ import com.ejie.x38.hdiv.interceptor.SecureModelAndViewInterceptor;
 @Configuration
 public class WebConfig extends WebMvcConfigurationSupport {
 
-//	@Autowired
-//	private HttpMessageConverter<?> msgConverter;
-//
-//	@Autowired
-//	private ConversionService conversionService;
-	
 	@Autowired
-	private SecureModelAndViewInterceptor secureModelAndViewInterceptor;
-
-//	@Autowired
-//	@Qualifier("validator")
-//	private LocalValidatorFactoryBean validatorFactoryBean;
-
-//	@Override
-//	@Bean(name = "requestMappingHandlerAdapter")
-//	public RequestMappingHandlerAdapter requestMappingHandlerAdapter() {
-//
-//		RequestMappingHandlerAdapter adapter = super.requestMappingHandlerAdapter();
-//
-//		ConfigurableWebBindingInitializer webBindingInitializer = new ConfigurableWebBindingInitializer();
-//		webBindingInitializer.setConversionService(conversionService);
-//		webBindingInitializer.setValidator(validatorFactoryBean);
-//		adapter.setWebBindingInitializer(webBindingInitializer);
-//
-//		List<HttpMessageConverter<?>> msgConverters = new ArrayList<HttpMessageConverter<?>>();
-//		msgConverters.add(msgConverter);
-//		adapter.setMessageConverters(msgConverters);
-//
-//		List<HandlerMethodArgumentResolver> argumentResolvers = new ArrayList<HandlerMethodArgumentResolver>();
-//		argumentResolvers.add(new RequestJsonBodyMethodArgumentResolver());
-//		adapter.setCustomArgumentResolvers(argumentResolvers);
-//
-//		return adapter;
-//	}
-	
+	private SecureModelAndViewInterceptor secureModelAndViewInterceptor;	
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		System.out.println("XAS----*****************************************");
-		System.out.println("XAS----*****************************************");
-		System.out.println("XAS----*****************************************");
-		System.out.println("XAS----*****************************************");
-		System.out.println("XAS----*****************************************");
-		System.out.println("XAS----*****************************************");
-		System.out.println("XAS----*****************************************");
-		System.out.println("XAS----*****************************************");
-	    registry.addInterceptor(secureModelAndViewInterceptor);
+		registry.addInterceptor(secureModelAndViewInterceptor);
 	}
 }
