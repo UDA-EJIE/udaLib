64f7da0cfb58b352c2fcb22d15d9342b60cb3b64
xabier_aldama <xabier.aldama@hdivsecurity.com>
2022-03-01 01:40:05 +0100

    Use MvcExceptionResolverConfig as WebMvcConfigurationSupport base

--- x38ShLibClasses/src/com/ejie/x38/hdiv/config/WebConfig.java
+++ x38ShLibClasses/src/com/ejie/x38/hdiv/config/WebConfig.java
@@ -3,18 +3,19 @@ package com.ejie.x38.hdiv.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
+import com.ejie.x38.control.exception.MvcExceptionResolverConfig;
 import com.ejie.x38.hdiv.interceptor.SecureModelAndViewInterceptor;
 
 @Configuration
-public class WebConfig extends WebMvcConfigurationSupport {
+public class WebConfig extends MvcExceptionResolverConfig {
 
 	@Autowired
 	private SecureModelAndViewInterceptor secureModelAndViewInterceptor;	
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
+		super.addInterceptors(registry);
 		registry.addInterceptor(secureModelAndViewInterceptor);
 	}
 }
