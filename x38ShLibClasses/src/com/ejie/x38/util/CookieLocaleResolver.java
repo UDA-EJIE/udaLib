4737e8b730bc3a26e32fed5b50e48bb109b57ebb
Xabi <xaabi6@users.noreply.github.com>
2022-06-13 14:46:43 +0200

    Limpiar importaciones no usadas
    Ninguna de las importaciones eliminadas estaba siendo usada.
    
    (cherry picked from commit a0db31cad7ff8b0d4c937a806d47bf05dec1138d)

--- x38ShLibClasses/src/com/ejie/x38/util/CookieLocaleResolver.java
+++ x38ShLibClasses/src/com/ejie/x38/util/CookieLocaleResolver.java
@@ -1,14 +1,5 @@
 package com.ejie.x38.util;
 
-import org.springframework.context.i18n.LocaleContext;
-import org.springframework.context.i18n.TimeZoneAwareLocaleContext;
-
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.Locale;
-import java.util.TimeZone;
-
 public class CookieLocaleResolver extends org.springframework.web.servlet.i18n.CookieLocaleResolver {
 
     @Override
