ddbb3ad8e8a8d6e5ec46863b6968606da69f5b0c
xabier_aldama <xabier.aldama@hdivsecurity.com>
2022-11-10 09:34:05 +0100

    Deleted unused code

--- x38ShLibClasses/src/com/ejie/x38/hdiv/util/Utils.java
+++ x38ShLibClasses/src/com/ejie/x38/hdiv/util/Utils.java
@@ -1,8 +1,6 @@
 package com.ejie.x38.hdiv.util;
 
 import java.lang.annotation.Annotation;
-import java.lang.reflect.Method;
-import java.lang.reflect.Parameter;
 
 public class Utils {
 	
@@ -10,19 +8,6 @@ public class Utils {
 		for(Annotation annotation : annotations) {
 			if(annotation.annotationType() == clazz) {
 				return annotation;
-			}
-		}
-		return null;
-	}
-	
-	public static Parameter findAnnotatedField(Method method, Class<?> clazz) {
-		
-		Annotation[][] anotations = method.getParameterAnnotations();
-		for (int i = 0; i < anotations.length; i++) {
-			for(Annotation annotation : anotations[i]) {
-				if(annotation.annotationType() == clazz) {
-					return method.getParameters()[i];
-				}
 			}
 		}
 		return null;
