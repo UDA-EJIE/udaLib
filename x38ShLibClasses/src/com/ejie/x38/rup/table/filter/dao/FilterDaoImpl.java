2a34c3a28413a42fd358a75861d5a1446f3c2abb
marcos9818 <131156200+marcos9818@users.noreply.github.com>
2023-12-21 14:56:21 +0100

    Transforma el string filterValue en un string JSON valido

--- x38ShLibClasses/src/com/ejie/x38/rup/table/filter/dao/FilterDaoImpl.java
+++ x38ShLibClasses/src/com/ejie/x38/rup/table/filter/dao/FilterDaoImpl.java
@@ -2,7 +2,11 @@ package com.ejie.x38.rup.table.filter.dao;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.sql.DataSource;
 
@@ -19,6 +23,10 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ejie.x38.rup.table.filter.model.Filter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 @Repository
@@ -57,9 +65,25 @@ public class FilterDaoImpl implements FilterDao{
 			filtro.setFilterName(defaultLobHandler.getClobAsString(resultSet,col_filterName));
 			filtro.setFilterUser(resultSet.getString(col_filterUser));
 			filtro.setFilterDefault(resultSet.getBoolean(col_filterDefault));
-			filtro.setFilterValue(defaultLobHandler.getClobAsString(resultSet,
-					col_filterValue));
-		
+			
+			String filterValue = defaultLobHandler.getClobAsString(resultSet,
+					col_filterValue);
+			
+			try {
+	            // Verificar si el String es JSON antes de realizar la conversión
+	            if (!isJson(filterValue)) {
+	            	String jsonString = convertToValidJson(filterValue);
+
+	              filtro.setFilterValue(jsonString);
+	            } else {
+	               filtro.setFilterValue(filterValue); 
+	            }
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }	    
+			
+			
+			
 			return filtro;
 		}
 	};
@@ -399,7 +423,74 @@ public class FilterDaoImpl implements FilterDao{
 	
 	
 	
+	private static boolean isJson(String str) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.readTree(str);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+	
+	// Método para convertir un String a formato JSON válido
+    private static String convertToValidJson(String inputString) {
+        // Dividir el string en pares key:value
+        String[] pairs = inputString.split(", ");
 
+        // Crear un mapa para almacenar las claves y valores
+        Map<String, String> keyValueMap = new HashMap();
+
+        // Expresión regular para identificar pares key:value
+        Pattern pattern = Pattern.compile("(\\w+):(\\S+)");
+        for (String pair : pairs) {
+            Matcher matcher = pattern.matcher(pair);
+            if (matcher.matches()) {
+                String key = matcher.group(1);
+                String value = matcher.group(2);
+
+                // Si la clave contiene la palabra "fecha", intentar identificar el formato y convertir
+                if (key.toLowerCase().contains("fecha")) {
+                    value = convertFecha(value);
+                } else {
+                    // Agregar comillas dobles solo si el valor no está entre comillas
+                    if (!value.matches("\"[^\"]*\"")) {
+                        value = "\"" + value + "\"";
+                    }
+                }
+
+                // Almacenar en el mapa
+                keyValueMap.put(key, value);
+            }
+        }
+
+        // Construir el JSON
+        StringBuilder jsonStringBuilder = new StringBuilder("{");
+        for (Map.Entry<String, String> entry : keyValueMap.entrySet()) {
+            jsonStringBuilder.append("\"").append(entry.getKey()).append("\":").append(entry.getValue()).append(",");
+        }
+        // Eliminar la coma final si hay al menos una entrada
+        if (!keyValueMap.isEmpty()) {
+            jsonStringBuilder.deleteCharAt(jsonStringBuilder.length() - 1);
+        }
+        jsonStringBuilder.append("}");
+
+        return jsonStringBuilder.toString();
+    }
+
+    // Método para convertir fechas al formato deseado
+    private static String convertFecha(String value) {
+    	try {
+            // Convertir el valor directamente a fecha
+            long milliseconds = Long.parseLong(value);
+            Date date = new Date(milliseconds);
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+            return "\"" + sdf.format(date) + "\"";
+        } catch (NumberFormatException e) {
+            // Si no se puede analizar la fecha, mantener el valor original entre comillas
+            return "\"" + value + "\"";
+        }
+    }
 
 }
 
