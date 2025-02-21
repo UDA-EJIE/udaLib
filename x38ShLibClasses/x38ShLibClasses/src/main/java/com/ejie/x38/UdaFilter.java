3af38dd6099334ec2eef2ee41e6a2a1998a50d9b
Xabi <xaabi6@users.noreply.github.com>
2025-02-21 08:18:38 +0100

    Codificar URI de pantalla de error
    Se codificará la URI en UTF-8 para evitar problemas que pueden surgir con algunos caracteres.
    
    (cherry picked from commit 4781c73295dd186e918daa7106f839db5c7d6bc5)

--- x38ShLibClasses/x38ShLibClasses/src/main/java/com/ejie/x38/UdaFilter.java
+++ x38ShLibClasses/x38ShLibClasses/src/main/java/com/ejie/x38/UdaFilter.java
@@ -15,6 +15,7 @@
 */
 package com.ejie.x38;
 
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -32,6 +33,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.DelegatingFilterProxy;
+import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.web.util.UriUtils;
 
 import com.ejie.x38.serialization.ThreadSafeCache;
 import com.ejie.x38.util.StackTraceManager;
@@ -171,7 +174,7 @@ public class UdaFilter extends DelegatingFilterProxy {
 						}
 					}
 
-					httpServletResponse.sendRedirect(error.toString());
+					httpServletResponse.sendRedirect(UriUtils.encodeQuery(error.toString(), StandardCharsets.UTF_8));
 				}
 			} catch (Exception exc) {
 				logger.error("Problem with sending of the response", exc);
